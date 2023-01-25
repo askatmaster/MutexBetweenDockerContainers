2da63134cc21e1e5bb48efa313f9f1d339d480d4
Askat Pazylidinov <stivendrak666@gmail.com>
2023-01-25 15:48:58 +0600

    Add test console project

---
+++ MutexDockerTest/MutexDockerTest/Program.cs
@@ -0,0 +1,9 @@
+﻿using MutexDockerTest;
+Console.WriteLine("Start");
+
+var obj1 = new TestAsync();
+var obj = new TestAsync();
+
+obj.TestMutex(obj1);
+
+Console.WriteLine("End");
---
+++ MutexDockerTest/MutexDockerTest/TestAsync.cs
@@ -0,0 +1,33 @@
+﻿namespace MutexDockerTest;
+
+public class TestAsync
+{
+    private async Task<string> GetString()
+    {
+        await Task.Delay(100);
+
+        return await Task.FromResult("Tasker");
+    }
+
+    public void TestMutex(TestAsync obj)
+    {
+        using var mutex = new Mutex (false, "Global\\mutex"); //Create Global Mutex
+
+        mutex.WaitOne();
+
+        try
+        {
+            var data1 =  obj.GetString().GetAwaiter().GetResult();
+            var data2 =  obj.GetString().GetAwaiter().GetResult();
+            var data3 =  obj.GetString().GetAwaiter().GetResult();
+            var data4 =  obj.GetString().GetAwaiter().GetResult();
+            Console.WriteLine("Hello");
+        }
+        finally
+        {
+            Console.WriteLine("ReleaseInit");
+            mutex.ReleaseMutex();
+            Console.WriteLine("Release");
+        }
+    }
+}
